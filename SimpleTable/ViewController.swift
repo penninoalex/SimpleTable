7c63710a2d54b464bea7ee2f02a2a92786eabf1f
Alessandro Pennino <alessandropennino@10-22-49-160.0200.par.it.iosda.org>
2017-12-19 09:57:36 +0100

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Alessandro Pennino on 19/12/17.
+//  Copyright © 2017 Alessandro Pennino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
